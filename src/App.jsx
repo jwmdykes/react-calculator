a9c89206a27cf6ddfac0be24d806caf3d88a605d
John Dykes <98johndykes@gmail.com>
2025-04-07 09:52:03 -0400

    simplify calculator

--- src/App.jsx
+++ src/App.jsx
@@ -274,8 +274,7 @@ function Calculator() {
 
 function App() {
   return (
-    <div className="bg-linear-to-br from-slate-800 to-slate-950  h-dvh w-full flex items-center justify-center relative overflow-clip">
-      <div className="w-[600px] h-[800px] absolute right-0 translate-x-1/2 translate-y-[200px] bg-slate-800 rounded-full blur-[150px]"></div>
+    <div className="bg-linear-to-br from-slate-800 to-slate-950 h-dvh flex items-center justify-center">
       <Calculator />
     </div>
   );
